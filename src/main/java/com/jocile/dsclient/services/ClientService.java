70121e2214390345273703852a83a5aabbfaa274
jocile <jocile@gmail.com>
2022-01-13 03:34:58 -0300

    Pagination

--- src/main/java/com/jocile/dsclient/services/ClientService.java
+++ src/main/java/com/jocile/dsclient/services/ClientService.java
@@ -10,6 +10,8 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class ClientService {
   @Autowired
   private ClientRepository repository;
+
+  @Transactional(readOnly = true)
+  public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+    Page<Client> list = repository.findAll(pageRequest);
+    return list.map(x -> new ClientDTO(x));
+  }
 
   @Transactional(readOnly = true)
   public List<ClientDTO> findAll() {
